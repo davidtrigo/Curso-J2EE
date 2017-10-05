616c855f93532fe1f674ecadd51fb10d3d828575
David <davidtrigo@github>
2017-10-05 21:44:52 +0200

    Cambios en Carta.java
    Baraja.java
    iniJuego()
      mesaAmazo()
      ponerEnMesa()
      getCartaSeleccionada()
      
      
      añadido imagenes de cartas de baraja española

--- Truco21/src/main/java/com/truco21/business/Baraja.java
+++ Truco21/src/main/java/com/truco21/business/Baraja.java
@@ -3,170 +3,178 @@ package com.truco21.business;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Baraja extends ArrayList<Carta>{
+public class Baraja{
 	
 
-	private static final long serialVersionUID = 1L;
+	private int status; 
+	private ArrayList<ArrayList<Carta>>  mesa; 
 	
-	ArrayList<Carta> paqueteCartas;
-	ArrayList<Carta> mazo21;
-	
+
 	
 	public Baraja() {
-		super();
-		
-	}
-	
-	
-	
-	public Baraja(ArrayList<Carta> baraja) {
-		super();
-		this.paqueteCartas = baraja;
+		super();	
+		status=0; 
 	}
 
 
-
-	/*
-	 *crearBaraja()                                                        
-	 * 
-	 * método que crea un ArrayList de objetos Carta
-	 * 
-	 */
-
-	
-	public ArrayList<Carta> crearBaraja(){ 
-		
-
-		String[] palo = {"corazon","diamantes","trebol","pica"};
-		String[] numero={"AS","K","Q","J","10","9","8","7","6","5","4","3","2"};
-		String imagen="";
-		
-		 paqueteCartas = new ArrayList<Carta>();
-		 
-		for (int i=0; i< palo.length;i++) {
+		public ArrayList<ArrayList<Carta>> iniJuego(){ 
+			status=1; 
+			ArrayList<Carta> mazo21 = iniMazo21();  
+			ArrayList<ArrayList<Carta>>  mesa = ponerEnMesa(mazo21); 
+			return mesa; 
+		}
 			
-			for (int j = 0; j < numero.length; j++) {
+		public ArrayList<ArrayList<Carta>> siguienteMesa(int select){ 
+			status++; 
+			ArrayList<Carta> mazo21= mesaAmazo(select,mesa); 
+			mesa = ponerEnMesa(mazo21); 
+			return mesa; 
+		}
+		
+		
+		/*
+		 *  select  valor que indica en que array esta la carta seleccionada
+		 *   0   primer array ,monton de la izquierda 
+		 *   1   primer array ,monton del centro
+		 *   2   primer array ,monton de la derecha
+		 * 
+		 * mazo21  array que addiciona 
+		 * 
+		 *   return   retorna un mazo
+		 * 
+		 */
+		private ArrayList<Carta>   mesaAmazo(int select, ArrayList<ArrayList<Carta>> mesa) {
+			
+			ArrayList<Carta>  mazo21 = new ArrayList<Carta> (); 
+			
+			if(select == 0){
 				
-				Carta carta = new Carta(palo, numero, imagen);
-				paqueteCartas.add(carta);	
-		
-			System.out.println(carta.numero[j]+" de "+carta.palo[i]);
-			
-			}
-		}
-		
-		return paqueteCartas;
-		 
-	}
-	
-	/*
-	 * barajar()
-	 * 
-	 * Método que baraja el mazo de cartas
-	 * 
-	 *de  la baraja creada  guarda en el Arraylist mazo21  21 objetos de tipo Carta
-	 * 
-	 * @return mazo21   retorna un arrayList de objetos Cartas  
-	 * */
-		public  ArrayList<Carta> barajar(){
-					
-			Baraja baraja = new Baraja();
-			baraja.crearBaraja();
-			mazo21 = new ArrayList<>();
-	
-			Random random = new Random();
-
-			// Mientras queden cartas en la baraja 
-			while (mazo21.size()<20){
-			   // Elegimos un índice al azar, entre 0 y el número de cartas que quedan por sacar
-			   int randomIndex = random.nextInt(baraja.size());
-
-			   // Guardamos la carta en nuevo arrayList de cartas
-			   mazo21.add(baraja.get(randomIndex));
-
-			   // Y eliminamos la carta del mazo (la borramos de la lista)
-			   baraja.remove(randomIndex);
-			}
-			
-			return mazo21;
-			
-		}
-		
-
-	/*
-	 * crearMazo()
-	 * 
-	 * Método que crea un mazo 
-	 * int <i>desde</i> indice del inicio del mazo
-	 * 
-	 * */
-		public ArrayList<Carta> crearMazo(int desde){
-			
-			ArrayList<Carta> mazo = new ArrayList<>();
-			int i;
-			
-			for (i = desde; i < (i+7); i++) {
+				mazo21.addAll(mesa.get(1)); 
+				mazo21.addAll(mesa.get(0)); 
+				mazo21.addAll(mesa.get(1)); 
 				
-				mazo.add(this.mazo21.get(i));
+			} else if (select == 1){
 				
+				mazo21.addAll(mesa.get(0));
+				mazo21.addAll(mesa.get(1));
+				mazo21.addAll(mesa.get(2));
+				
+			}else if (select == 2){
+				
+				mazo21.addAll(mesa.get(1));
+				mazo21.addAll(mesa.get(2));
+				mazo21.addAll(mesa.get(0));
+			
+			} else {
+				System.out.println("Ningun mazo seleccionado");
 			}
-		
-			return mazo;
+
+			return mazo21; 
 		}
+
+
+
+
+		private ArrayList<ArrayList<Carta>> ponerEnMesa(ArrayList<Carta> mazo21) {
+			 
+			ArrayList<ArrayList<Carta>> mesa = new   ArrayList<ArrayList<Carta>>();   
+			ArrayList<Carta> mazo1 = new ArrayList<Carta>();
+			ArrayList<Carta> mazo2 = new ArrayList<Carta>();
+			ArrayList<Carta> mazo3 = new ArrayList<Carta>();
+			
+			
+			
+			/**TODO  hacer bucle
+			 * repartir carta por mazo
+			 * mazo1: 0  3 5  8
+			 * mazo2: 1  4  6  9 
+			 * mazo3: 2  5  7  10 ...
+			 * */
+			
+			
+			/* añade los mazos creados a la mesa*/
+			mesa.add(mazo1);
+			mesa.add(mazo1);
+			mesa.add(mazo3);
+			
+			
+			
+			
+			return mesa;
+	     }
 		
 		
-	public ArrayList<Carta> seleccionarMazo (String monton){
 		
-		ArrayList<Carta> mazo1  = new ArrayList<>();
-		ArrayList<Carta> mazo2  = new ArrayList<>();
-		ArrayList<Carta> mazo3  = new ArrayList<>();
-		ArrayList<Carta> mazoTemp  = new ArrayList<>();
-		Baraja baraja = new Baraja();
+		/*
+		 * 
+		 * Selecciona segundo mazo get(1) y de ese ,mazo la carta que hace 11 get(4)
+		 * 
+		 * */
+	     public Carta getCartaSeleccionada(){
+	    	 mesa.get(1).get(4); 
+	    	  
+	    	 
+//	    	 Carta carta = new Carta(palo, numero);
+	    	
+	    	 return null;
+	     }
 		
-		mazo1 = baraja.crearMazo(0);
-		mazo2=  baraja.crearMazo(7);
-		mazo3= baraja.crearMazo(13);
 		
-		if (monton.equals("I")){ // mazo de la izquierda
+		
+		/*
+		 *crearBaraja()                                                        
+		 * 
+		 * método que crea un ArrayList de objetos Carta
+		 * 
+		 */
+
+		
+		private static ArrayList<Carta> crearBaraja(){ 
 			
-			mazoTemp= mazo1;
-			mazo1=mazo2;
-			mazo2=mazoTemp;
-			mazo3=mazo3;
+			String[] palo = {"oro","basto","espada","copa"};
+			String[] numero={"12","11","10","9","8","7","6","5","4","3","2","1"};
 			
-		 
-			
-		} else if  (monton.equals("C")){  // mazo Central
-			
-			mazoTemp= mazo2;
-			
-			mazo1=mazo1;
-			mazo2=mazoTemp;
-			mazo3=mazo3;
-			
-			
-			
-			
-			
-		} else if (monton.equals("D")){  // mazo de la dercha
-			
-			mazoTemp =mazo3;
-			mazo1=mazo3;
-			mazo2=mazoTemp;
-			mazo3=mazo3;
-			
-			
+			 ArrayList<Carta> paqueteCartas = new ArrayList<Carta>(); 
+			 
+			for (int i=0; i< palo.length;i++) {		
+				for (int j = 0; j < numero.length; j++) {			
+					Carta carta = new Carta(palo[i], numero[j]);
+					paqueteCartas.add(carta);						
+				}
+			}		
+		 return paqueteCartas;		 
 		}
-		
-		
-		
-		return mazoTemp;
-	}
 		
 
-	
-	 
-		 
+		/*
+		 * barajar()
+		 * 
+		 * Método que baraja el mazo de cartas
+		 * 
+		 *de  la baraja creada  guarda en el Arraylist mazo21  21 objetos de tipo Carta
+		 * 
+		 * @return mazo21   retorna un arrayList de objetos Cartas  
+		 * */
+			private  static ArrayList<Carta> iniMazo21(){
+				
+				ArrayList<Carta> mazo21 = new ArrayList<>();
+				ArrayList<Carta> baraja = crearBaraja();			
+				Random random = new Random();
+
+				// Mientras queden cartas en la baraja 
+				while (mazo21.size()<20){
+				   // Elegimos un índice al azar, entre 0 y el número de cartas que quedan por sacar
+				   int randomIndex = random.nextInt(baraja.size());
+
+				   // Guardamos la carta en nuevo arrayList de cartas
+				   mazo21.add(baraja.get(randomIndex));
+
+				   // Y eliminamos la carta del mazo (la borramos de la lista)
+				   baraja.remove(randomIndex);
+				}			
+				return mazo21;
+			}
+				 
 
 }
 
--- Truco21/src/main/java/com/truco21/business/Carta.java
+++ Truco21/src/main/java/com/truco21/business/Carta.java
@@ -4,23 +4,14 @@ package com.truco21.business;
 public class Carta {
 	
 	
-	String[] palo = {"oro","basto","espadas","copas"};
-	String[] numero={"R","S","C","9","8","7","6","5","4","3","2","1"};
-	String imagen="";
+	private String palo;
+	private String numero;
 	
-	
-	
-	
-	  public Carta(String[] palo, String[] numero, String imagen) {
+	  public Carta(String palo, String numero) {
 		super();
 		this.palo = palo;
 		this.numero = numero;
-		this.imagen = imagen;
 	}
-	
-	
-	
-	
-	
+
 
 }
